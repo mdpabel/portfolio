4e9ede1f7abb6bc5268aae0696e44e75017689fa
MD Pabel <mdpabel385@gmail.com>
2023-04-21 18:09:41 +0600

    Fixed logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,9 @@ const Header = ({
   return (
     <header className='relative flex items-center justify-between h-16 max-w-6xl px-8 mx-auto mt-10 '>
       <div className='text-2xl text-gray-600'>
-        <Link href='/'>{'<Pabel />'}</Link>
+        <Link onClick={() => setOpen(false)} href='/'>
+          {'<Pabel />'}
+        </Link>
       </div>
 
       <div>

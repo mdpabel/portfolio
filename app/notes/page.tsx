156fab69ebe10b9b4524d7d99954d51bd674853b
MD_Pabel <mdpabel385@gmail.com>
2024-03-28 22:38:09 +0600

    build: search functionality

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { getNotes } from '@/lib/notes';
 import Search from './Search';
 
-export const dynamic = 'force-static';
+// export const dynamic = 'force-static';
 
 type PageProps = {
   searchParams: {

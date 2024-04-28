354213a6604f81fd4216807afe7e152bb6764a9e
MD_Pabel <mdpabel385@gmail.com>
2024-04-28 20:56:32 +0600

    update: next version

--- app/api/webhooks/github/route.ts
+++ app/api/webhooks/github/route.ts
@@ -2,8 +2,6 @@ import { revalidatePath, revalidateTag } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const POST = async (req: NextRequest) => {
-  console.log(req);
-  await revalidateTag('notes');
-  await revalidatePath('/notes');
+  revalidateTag('notes');
   return NextResponse.json({});
 };

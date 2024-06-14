f037fb0be5af79a405bbae7aede3891333b15d92
mdpabel <mdpabel385@gmail.com>
2024-06-14 10:10:16 +0600

    refactor: update revalidation method in POST handler refactor: update revalidation method in POST handler

--- app/api/webhooks/github/route.ts
+++ app/api/webhooks/github/route.ts
@@ -2,6 +2,9 @@ import { revalidatePath, revalidateTag } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const POST = async (req: NextRequest) => {
-  revalidateTag('notes');
+  // revalidateTag('notes');
+   revalidatePath(path);
+  revalidatePath(`/${path}/[slug]`, 'page');
+  
   return NextResponse.json({});
 };

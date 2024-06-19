d80be60fff7924542465ad886b2bfddc3b39d697
mdpabel <mdpabel385@gmail.com>
2024-06-19 18:54:21 +0600

    chore: view count no store

--- components/note/View.tsx
+++ components/note/View.tsx
@@ -1,12 +1,14 @@
 import { getNoteView } from '@/utils/notes';
 import Image from 'next/image';
 import videIcon from '@/public/videoIcon.png';
+import { unstable_noStore as noStore } from 'next/cache';
 
 const View = ({ title }: { title: string }) => {
+  noStore();
   const totalViews = getNoteView(title);
 
   return (
-    <div className='hidden md:flex md:space-x-2 text-gray-500 text-sm line-clamp-1'>
+    <div className='md:flex md:space-x-2 hidden line-clamp-1 text-gray-500 text-sm'>
       <span>{totalViews} views</span>
       <Image src={videIcon} alt='video icon' width={20} height={15} />
     </div>

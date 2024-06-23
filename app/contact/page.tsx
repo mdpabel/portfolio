81de806b09559c3a8a03d92402d9a85c8c3f5f7c
mdpabel <mdpabel385@gmail.com>
2024-06-23 16:14:51 +0600

    feat: contact-page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,53 +1,30 @@
+import Script from 'next/script';
 import React from 'react';
-import {
-  FBIcon,
-  LocationIcon,
-  PhoneIcon,
-  TwitterIcon,
-  EmailIcon,
-} from '@/components/ui/Icons';
-import ContactForm from './ContactForm';
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Contact',
-};
-
-export const dynamic = 'force-static';
-
-const Contact = () => {
+const page = () => {
   return (
-    <>
-      <div className='mx-auto pt-12 md:pt-20 max-w-5xl'>
-        <div className='gap-10 grid grid-cols-1 md:grid-cols-2'>
-          <div className='flex flex-col justify-center items-center bg-white/50 p-5 rounded-lg'>
-            <div className='space-y-4'>
-              <h2 className='mb-4 font-semibold text-4xl text-gray-600'>
-                Contact Me
-              </h2>
-              <div className='flex gap-4'>
-                <LocationIcon />
-                <span>Cumilla, BD</span>
-              </div>
-
-              <div className='flex gap-4'>
-                <PhoneIcon />
-                <span>+8801761442268</span>
-              </div>
-
-              <div className='flex gap-4'>
-                <EmailIcon />
-                <span>mdpabel385@gmail.com</span>
-              </div>
-            </div>
-          </div>
-          <div>
-            <ContactForm />
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+      <Script
+        src='https://tally.so/widgets/embed.js'
+        type='text/javascript'></Script>
+      <iframe
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          border: 0,
+        }}
+        data-tally-src='https://tally.so/r/w8kbqP?transparentBackground=1'
+        width='100%'
+        height='100%'
+        frameBorder='0'
+        marginHeight={0}
+        marginWidth={0}
+        title='Hire Me – Your Next Top Talent'></iframe>
+    </div>
   );
 };
 
-export default Contact;
+export default page;
